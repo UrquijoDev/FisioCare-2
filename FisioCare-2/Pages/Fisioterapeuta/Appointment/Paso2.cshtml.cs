480920fadb335ee87c826ca9b5759221c5a7aeef
UrquijoDev <urquijopedro790@gmail.com>
2025-05-17 13:21:09 -0700

    Paso 2, Fisioterapeuta ya quedo

--- FisioCare-2/Pages/Fisioterapeuta/Appointment/Paso2.cshtml.cs
+++ FisioCare-2/Pages/Fisioterapeuta/Appointment/Paso2.cshtml.cs
@@ -5,8 +5,40 @@ namespace FisioCare_2.Pages.Fisioterapeuta.Appointment
 {
     public class Paso2Model : PageModel
     {
-        public void OnGet()
+
+
+        [BindProperty(SupportsGet = true)]
+        public string FisioterapeutaId { get; set; } = string.Empty;
+
+        [BindProperty(SupportsGet = true)]
+        public int ServicioId { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string PacienteID { get; set; } = string.Empty;
+
+        [BindProperty]
+        public DateTime FechaSeleccionada { get; set; }
+        public void OnGet(string fisioterapeutaId, int servicioId, string pacienteId)
         {
+            FisioterapeutaId = fisioterapeutaId;
+            ServicioId = servicioId;
+            PacienteID = pacienteId;
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                Console.WriteLine("Entre aqui por menso");
+                return Page();
+            }
+
+            return RedirectToPage("Paso3", new
+            {
+                fisioterapeutaId = FisioterapeutaId,
+                servicioId = ServicioId,
+                idPaciente = PacienteID,
+                fecha = FechaSeleccionada.ToString("yyyy-MM-dd")
+            });
         }
     }
 }
