3406944b6f7c18dff93db9ece90194bc051dfca4
UrquijoDev <urquijopedro790@gmail.com>
2025-05-17 12:57:26 -0700

    Paso 1 Fisioterapeuta completado

--- FisioCare-2/Pages/Fisioterapeuta/Appointment/Paso1.cshtml.cs
+++ FisioCare-2/Pages/Fisioterapeuta/Appointment/Paso1.cshtml.cs
@@ -1,3 +1,6 @@
+using FisioCare_2.Models;
+using FisioCare_2.Services;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +8,28 @@ namespace FisioCare_2.Pages.Fisioterapeuta.Appointment
 {
     public class Paso1Model : PageModel
     {
-        public void OnGet()
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly ApplicationDbContext _context;
+
+        public Paso1Model(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager, ApplicationDbContext context)
         {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _context = context;
         }
+        [BindProperty(SupportsGet = true)]
+        public string FisioterapeutaId { get; set; } = string.Empty;
+
+        [BindProperty(SupportsGet = true)]
+        public string PacienteID { get; set; }
+        public List<Servicio> Servicios { get; set; } = new();
+        public void OnGet(string idFisioterapeuta, string idPaciente)
+        {
+            FisioterapeutaId = idFisioterapeuta;
+            PacienteID = idPaciente;
+            Servicios = _context.Servicio.ToList();
+        }
+
     }
 }
